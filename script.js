966da16374189c2167836fd1b47a54957232d46f
Kem13 <mathew.kempke@yahoo.com>
2021-04-28 12:05:02 -0500

    closest point to origin

--- script.js
+++ script.js
@@ -189,7 +189,7 @@ function find_single_number(arr) {
 
 console.log(find_single_number([1,4,2,1,3,2,3]))
 
-//
+// Find top numbers 
 
 const Heap = require('./collections/heap'); //http://www.collectionjs.com
 
@@ -211,4 +211,66 @@ function find_k_largest_numbers(nums, k) {
 
     // the heap has the top 'K' numbers, return them in a list
     return minHeap.toArray();
+}
+
+// find smallest numbers
+// Time complexity = O(K * logK + (N - K) * logK) which is asymptotically equal to O(N * logK)
+const Heap = require('./collections/heap'); //http://www.collectionjs.com
+
+function find_Kth_smallest_number(nums, k) {
+    maxHeap = new Heap();
+    // put first k numbers in the max heap
+    for (let i = 0; i < k; i++) {
+        maxHeap.push(nums[i]);
+    }
+
+    // go through the remaining numbers of the array
+    for (let i = k; i < nums.length; i++) {
+        if (nums[i] < maxHeap.peek()) {
+            maxHeap.pop();
+            maxHeap.push(nums[i]);
+        }
+    }
+
+    return maxHeap.peek();
+}
+//  "K" Closest Point to the Origin 
+const Heap = require('./collections/heap'); //http://www.collectionjs.com
+
+class Point {
+    constructor(x,y) {
+        this.x = x;
+        this.y = y;
+    }
+    // use for max-heap
+    compare(other) {
+        return this.distance_from_origion() - other.distance_from_origion();
+    }
+    distance_from_origion() {
+        // ignoring sqrt to calculate the distance
+        return (this.x * this.x) + (this.y * this.y);
+    }
+    print_point() {
+        process.stdout.write(`[${this.x}, ${this.y}]`);
+    }
+}
+
+function find_closest_points (points, k) {
+    const maxHeap = new Heap([], null, ((a,b,) => a.compare(b)));
+    // put first 'k' points in the max heap
+    for (i = 0; i < k; i++) {
+        maxHeap.push(points[i]);
+    }
+
+    // go through the remaining points of the input array, if a point is close3r to the origin than the top point 
+    // of the max-heap, remove the top point from heap and add the point from the input array
+
+for (i = k; i < points.length; i++) {
+    if (points[i].distance_from_origion < maxHeap.peek().distance_from_origion()) {
+        maxHeap.pop();
+        maxHeap.push(points[i])
+    }
+}
+    // the heap has 'k' points closest to the origin, return them in a list
+    return maxHeap.toArray();
 }
