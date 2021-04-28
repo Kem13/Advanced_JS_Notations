d50f6411eaade94e8e4c49bf200465fec39a8e53
Kem13 <mathew.kempke@yahoo.com>
2021-04-28 09:40:57 -0500

    Heap algorithms

--- script.js
+++ script.js
@@ -188,3 +188,27 @@ function find_single_number(arr) {
 }
 
 console.log(find_single_number([1,4,2,1,3,2,3]))
+
+//
+
+const Heap = require('./collections/heap'); //http://www.collectionjs.com
+
+
+function find_k_largest_numbers(nums, k) {
+    const minHeap = new Heap([], null, ((a,b) => b - a));
+    // put first 'K' numbers in the min heap
+    for (let i = 0; i < k; i++) {
+        minHeap.push(nums[i]);
+    }
+
+    // go through the remaining numbers of the array, if the number from the array is bigger than the top(i.e., smallest) number of the min-heap, remove the top number from heap and add the number from array
+    for (let i = k; i < nums.length; i++) {
+        if (nums[i] > minHeap.peek()) {
+            minHeap.pop();
+            minHeap.push(nums[i]);
+        }
+    }
+
+    // the heap has the top 'K' numbers, return them in a list
+    return minHeap.toArray();
+}
